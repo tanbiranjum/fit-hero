ac2c77ebe26e312a779a9fdcbf63c87eedb4ac47
tanbiranjum <tanbiranjum@hotmail.com>
2022-09-29 01:12:13 +0600

    break button added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,6 +5,11 @@ const Card = ({ data }) => {
   return (
     <div className="card">
       <img src={`img/${data.image}`} alt="" className="card__img" />
+      <h3 className="card__img">{data.name}</h3>
+      <p className="card__description"></p>
+      <p className="card__age">For Age: ${data.age}</p>
+      <p className="card__time">Time Required: ${data.time}s</p>
+      <button className="card__btn">Add to list</button>
     </div>
   );
 };
--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import profilepic from "../../assets/profile.jfif";
+import "./Drawer.css";
 
 const Drawer = () => {
-  return <div>Drawer</div>;
+  const [time, setTime] = useState(0);
+  const [breakTime, setBreakTime] = useState(0);
+  return (
+    <div className="drawer">
+      <div className="profile">
+        <img src={profilepic} alt="image" className="profile__img" />
+        <div className="profile-info">
+          <h3>Tanbir Anjum</h3>
+          <p>
+            <FaMapMarkerAlt /> Dhaka, Bangladesh
+          </p>
+        </div>
+      </div>
+      <p>Add A Break</p>
+      <div className="break-btn">
+        <button className="break-btn__btn">10s</button>
+        <button className="break-btn__btn">20s</button>
+        <button className="break-btn__btn">30s</button>
+        <button className="break-btn__btn">40s</button>
+        <button className="break-btn__btn">50s</button>
+      </div>
+      <p>Exercise Details</p>
+      <div className="time">
+        <h3 className="time__heading">Exercise time</h3>
+        <p className="time__count">{time} seconds</p>
+      </div>
+      <div className="break">
+        <h3 className="break__heading">Break time</h3>
+        <p className="break__count">{breakTime} seconds</p>
+      </div>
+    </div>
+  );
 };
 
 export default Drawer;
