ee699b3b263a1b6956a45c5e99cd730b78bc0843
tanbiranjum <tanbiranjum@hotmail.com>
2022-09-28 22:55:32 +0600

    Header Drawer components added and lato font added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FaCloudsmith } from "react-icons/fa";
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <FaCloudsmith />
+      <h1>Fit Hero</h1>
+    </div>
+  );
+};
+
+export default Header;
